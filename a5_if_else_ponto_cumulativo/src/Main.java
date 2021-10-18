4bbbfeedb8dc16a774eb7c5ece4c20c69b3b971a
João Fernando <70722165+jfmoraeslara@users.noreply.github.com>
2021-10-18 12:55:56 -0300

    a5 ponto cumulativo

---
+++ a5_if_else_ponto_cumulativo/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// URI 1051 ponto cumulativo
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double salario = sc.nextDouble();
+		
+		double imposto = 0.0; // (= 0.0) inicialização para rodar a variável por causa do condicional if
+		if (salario <= 2000.00) {
+			imposto = 0.00;
+		} else if (salario <= 3000.00) {
+			imposto = (salario - 2000.00) * 0.08;
+		} else if (salario <= 4500.00) {
+			imposto = (salario - 3000.00) * 0.18 + 1000.00 * 0.08;
+		} else {
+			imposto = (salario - 4500.00) * 0.28 + 1500.00 * 0.18 + 1000.00 * 0.08;
+		}
+
+		if (imposto == 0.00) {
+			System.out.println("Isento");
+		} else {
+			System.out.printf("R$ %.2f%n", imposto);
+		}
+
+		sc.close();
+	}
+}
+
