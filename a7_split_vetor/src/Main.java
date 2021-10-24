0df412480498f09fe03709cd728bdb26c4b6b0c8
João Fernando <70722165+jfmoraeslara@users.noreply.github.com>
2021-10-24 09:51:15 -0300

    a7 split vetor como separar um vetor

---
+++ a7_split_vetor/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {		
+		
+		String s = "potato-apple-lemon";
+		
+		String[] vect = s.split("-"); //usando o traço como separador
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);		
+	}
+}
