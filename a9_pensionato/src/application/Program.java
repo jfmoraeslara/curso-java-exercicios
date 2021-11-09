f90dbfdbdc23171c1f78d0d24ffadee324a4df49
João Fernando <70722165+jfmoraeslara@users.noreply.github.com>
2021-11-09 10:11:10 -0300

    a9 pensionato reserva de quartos

---
+++ a9_pensionato/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Scanner;
+import entities.Quartos;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Quartos[] vect = new Quartos[10];
+		
+		System.out.print("How many rooms will be rented? ");
+		int n = sc.nextInt();			
+		
+		for (int i=1; i<=n; i++) {			
+			System.out.println("Rent #" + i + ":");
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			vect[room] = new Quartos(name, email);
+			System.out.println();
+		}
+		
+		System.out.println("Busy rooms:");
+		for (int i=0; i<10; i++) {
+			// verificar se a posição está ou não ocupada
+			if (vect[i] != null) {
+				System.out.println(i + ": " + vect[i]);
+			}
+		}
+		
+		sc.close();
+	}
+}
